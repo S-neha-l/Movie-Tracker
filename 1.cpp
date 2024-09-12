e03c7e83eef38a24b988be7159706a2542024cb4
S-neha-l <sneh.al9478@gmail.com>
2024-09-12 13:56:49 +0530

    First Commit

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    vector<int> n;
+    n.push_back(3);
+    n.push_back(5);
+    for(auto x: n){
+        cout<<x<<endl;
+    }
+    return 0;
+}
